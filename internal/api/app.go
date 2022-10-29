da6dc08ca47fcce231e157dcd2e14d89176ab10c
rocky2010aaa@gmail.com <rocky2010aaa@gmail.com>
2022-10-29 11:49:50 +0900

    Refactoring source code

--- internal/api/app.go
+++ internal/api/app.go
@@ -1,8 +1,25 @@
 package api
 
 import (
+	"fmt"
+	"os"
+
+	"gorm.io/gorm"
+
+	"gitlab.com/gitlab.bbdev.team/vh/broadcast-subtitles/internal/config"
 	"gitlab.com/gitlab.bbdev.team/vh/broadcast-subtitles/internal/pkg/database"
 )
 
-func NewApp() {
+type App struct {
+	Database *gorm.DB
+}
+
+func NewApp() *App {
+	profile := fmt.Sprintf("config_%s", os.Getenv("BSSVR_PROFILE"))
+	config.SetConfig(profile)
+	config := config.GetConfig()
+	app := App{
+		Database: database.New(config.Postgres.Url),
+	}
+	return &app
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,5 +1,12 @@
 package config
 
+import (
+	"github.com/spf13/viper"
+	"log"
+)
+
+var configuration = Config{}
+
 type Config struct {
 	Port     int       `yaml:"port"`
 	Postgres *Postgres `yaml:"postgres"`
@@ -7,4 +14,37 @@ type Config struct {
 
 type Postgres struct {
 	Url string `yaml:"url"`
+}
+
+func SetConfig(profile string) error {
+	viper.AddConfigPath("../..")
+	viper.SetConfigName(profile)
+	viper.SetConfigType("yaml")
+	err := viper.ReadInConfig()
+	if err != nil {
+		return err
+	}
+	err = viper.Unmarshal(&Configuration)
+	if err != err {
+		return err
+	}
+	viper.OnConfigChange(func(e fsnotify.Event) {
+		log.Println("Config file changed:", e.Name)
+		err := viper.ReadConfig()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		err = viper.Unmarshal(&configuration)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+	})
+	viper.WatchConfig()
+}
+
+func GetConfig() *Config {
+	return &configuration
 }
