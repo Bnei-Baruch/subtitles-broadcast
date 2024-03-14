50bdef3e20dcb9885be460112d716ac34aaba046
Donggeon Lee <rocky2010aaa@gmail.com>
2024-03-14 17:17:02 +0900

    updating custom tab

--- src/Redux/NewSlide/NewSlide.js
+++ src/Redux/NewSlide/NewSlide.js
@@ -5,17 +5,26 @@ const API = process.env.REACT_APP_API_BASE_URL;
 
 const API_URL = {
   SetCustomSlideBySource: "custom_slide_by_source",
+  GetSlideLanguages: "slide_language",
 };
 
 export const SetCustomSlideBySource = createAsyncThunk(
   `/${API_URL.SetCustomSlideBySource}`,
   async (data, thunkAPI) => {
-    const requestBody = {
+      const requestBody = {
         // Your request body data here
         languages: [data.languages],
         slides: data.slides
-    };
-    const response = await axios.post(`${API}custom_slide`, requestBody);
-    return response.data;
+        };
+      const response = await axios.post(`${API}custom_slide`, requestBody);
+      return response.data;
+  }
+);
+
+export const GetSlideLanguages = createAsyncThunk(
+  `/${API_URL.GetSlideLanguages}`,
+  async (data, thunkAPI) => {
+      const response = await axios.get(`${API}slide_language`);
+      return response.data;
   }
 );
