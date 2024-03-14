090ac673c188f8dc740a2ae106bc24b67edb4930
Donggeon Lee <rocky2010aaa@gmail.com>
2024-03-14 13:05:22 +0900

    updating From KabbalahMedia tab

---
+++ src/Redux/NewSlide/NewSlide.js
@@ -0,0 +1,21 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const API = process.env.REACT_APP_API_BASE_URL;
+
+const API_URL = {
+  SetSlideLanguage: "slide_language",
+};
+
+export const SetSlideLanguages = createAsyncThunk(
+  `/${API_URL.SetSlideLanguage}`,
+  async (data, thunkAPI) => {
+    const requestBody = {
+        // Your request body data here
+        languages: [data.languages],
+        slides: data.slides
+    };
+    const response = await axios.post(`${API}custom_slide`, requestBody);
+    return response.data;
+  }
+);
