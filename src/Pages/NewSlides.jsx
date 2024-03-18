57538ef724548d74a74fcb28cb433cbfc0bcc0bb
bhumin-devsatva <154416236+bhumin-devsatva@users.noreply.github.com>
2024-03-19 00:54:39 +0530

    source autocomplete

--- src/Pages/NewSlides.jsx
+++ src/Pages/NewSlides.jsx
@@ -1,23 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./PagesCSS/Newslide.css";
 import Select from "react-select";
 import SlideSplit from "../Utils/SlideSplit";
+import {
+  ArchiveAutoComplete,
+  GetAllArchiveData,
+  getAutocompleteSuggetion,
+} from "../Redux/ArchiveTab/ArchiveSlice";
+import { useDispatch, useSelector } from "react-redux";
+import useDebounce from "../Services/useDebounce";
 
 const NewSlides = () => {
+  const dispatch = useDispatch();
   const [tagList, setTagList] = useState([]);
   //const [updateTagList, setUpdateTagList] = useState([]);
-  const [sourceUrl, setSourceUrl] = useState('');
-
+  const [sourceUrl, setSourceUrl] = useState("");
+  const [showAutocompleteBox, setShowAutocompleteBox] = useState(false);
+  const ActocompleteList = useSelector(getAutocompleteSuggetion);
   const [activeButton, setActiveButton] = useState("button1");
-
+  const DebouncingFreeText = useDebounce(sourceUrl, 500);
   const handleClick = (button) => {
     setActiveButton(button);
     // Add logic here to handle button click events
   };
 
-  const handleInputChange = (e) => {
-    setSourceUrl(e.target.value);
-  };
+  useEffect(() => {
+    dispatch(ArchiveAutoComplete({ query: sourceUrl }));
+  }, [DebouncingFreeText]);
 
   const loadSlides = () => {
     const docUrl = `${sourceUrl}`;
@@ -29,17 +38,23 @@ const NewSlides = () => {
           throw new Error(`Fetch failed with status ${response.status}`);
         }
         const data = await response.text();
-        const doc = parser.parseFromString(data, 'text/html');
+        const doc = parser.parseFromString(data, "text/html");
         // Extract text content from tags, for example, from all paragraphs
-        const contentElements = doc.querySelectorAll('h1,p');
-        const paragraphArray = Array.from(contentElements).map(element => ({
+        const contentElements = doc.querySelectorAll("h1,p");
+        const paragraphArray = Array.from(contentElements).map((element) => ({
           tag: element.tagName,
           content: element.outerHTML,
-        }));;
+        }));
         let tags = [];
-        paragraphArray.forEach(elementInfo => {
+        paragraphArray.forEach((elementInfo) => {
           const tagName = elementInfo.tag;
-          const wordArray = elementInfo.content.replace(/<[^>]*>/g, '').replace(/\n/g, '').trim().split('  ').join(' ').split(/(\s+)/);
+          const wordArray = elementInfo.content
+            .replace(/<[^>]*>/g, "")
+            .replace(/\n/g, "")
+            .trim()
+            .split("  ")
+            .join(" ")
+            .split(/(\s+)/);
           wordArray.forEach((word, index) => {
             const elementObject = {
               paragraphStart: false,
@@ -54,7 +69,7 @@ const NewSlides = () => {
         });
         setTagList(tags);
       } catch (error) {
-        console.error('Error fetching or parsing data:', error.message);
+        console.error("Error fetching or parsing data:", error.message);
       }
     };
     fetchData();
@@ -143,12 +158,47 @@ const NewSlides = () => {
           <div className="row m-4">
             <label>Language</label>
             <p>English</p>
-            <div className="input-box ">
-              <label className="w-100">Source Path</label>
+            <label className="w-100">Source Path</label>
+            <div className="form-group  autoComplete">
+              <input
+                className="form-control"
+                type="type"
+                value={sourceUrl}
+                onBlur={() => setShowAutocompleteBox(false)}
+                onKeyDown={(e) => {
+                  e.key === "Enter" &&
+                    dispatch(
+                      GetAllArchiveData({
+                        language: "en",
 
-              <input className="form-control" type="type" value={sourceUrl} onChange={handleInputChange} />
+                        keyword: sourceUrl,
+                      })
+                    );
+                }}
+                onChange={(e) => {
+                  setShowAutocompleteBox(true);
+                  setSourceUrl(e.target.value);
+                }}
+              />
+              {showAutocompleteBox && (
+                <ul class="suggestions" id="suggestions">
+                  {ActocompleteList?.map((suggestion, index) => (
+                    <li
+                    // key={index}
+                    // onClick={() => handleSuggestionClick(suggestion)}
+                    >
+                      {suggestion.source_value}
+                    </li>
+                  ))}
+                </ul>
+              )}
             </div>
-            <button className="btn btn-primary btn-sm col-3 m-4" onClick={loadSlides}>Add</button>
+            <button
+              className="btn btn-primary btn-sm col-3 m-4"
+              onClick={loadSlides}
+            >
+              Add
+            </button>
             <div>
               <SlideSplit tags={tagList} visible={true} />
             </div>
