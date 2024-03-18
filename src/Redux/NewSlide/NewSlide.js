3d48eea459b122563277e4b4f5cbc5485da2625a
Donggeon Lee <rocky2010aaa@gmail.com>
2024-03-18 13:34:20 +0900

    updating tab handling

--- src/Redux/NewSlide/NewSlide.js
+++ src/Redux/NewSlide/NewSlide.js
@@ -11,32 +11,21 @@ const API_URL = {
 export const SetCustomSlideBySource = createAsyncThunk(
   `/${API_URL.SetCustomSlideBySource}`,
   async (data, thunkAPI) => {
-    let requestBody;
     let languages;
-    if (data.languages.length > 1) {
-      languages = data.languages
+    if (typeof data.languages === "string") {
+      languages = [data.languages];
     } else {
-      languages = [data.languages]
+      languages = data.languages;
     }
-
-    if (data.name.length > 0) {
-      requestBody = {
-        // Your request body data here
-        file_name: data.name,
-        languages: languages,
-        source_uid: data.source_uid,
-        file_uid: data.file_uid,
-        slides: data.slides
-        };
-    } else {
-      requestBody = {
-        // Your request body data here
-        languages: languages,
-        source_uid: data.source_uid,
-        file_uid: data.file_uid,
-        slides: data.slides
-        };
-    }
+    const requestBody = {
+      // Your request body data here
+      file_name: data.name,
+      languages: languages,
+      source_uid: data.source_uid,
+      file_uid: data.file_uid,
+      slides: data.slides
+    };
+    
     const response = await axios.post(`${API}custom_slide`, requestBody);
     return response.data;
   }
