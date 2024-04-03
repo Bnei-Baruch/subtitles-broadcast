f68389e152fb482094481bced696b0741f140061
slavapa <slavapas13@gmail.com>
2024-04-03 13:09:49 +0300

    Refactoring

--- src/Components/QuestionMessage.jsx
+++ src/Components/QuestionMessage.jsx
@@ -1,50 +1,61 @@
 import React, { useState, useContext, useEffect } from "react";
 import AppContext from "../AppContext";
 import PropTypes from "prop-types";
-import mqttClientUtils, { parseMqttMessage } from "../Utils/MqttUtils";
-//import useMqtt, { parseMqttMessage } from "../Utils/UseMqttUtils";
+// import mqttClientUtils, { parseMqttMessage } from "../Utils/MqttUtils";
+import useMqtt, { parseMqttMessage } from "../Utils/UseMqttUtils";
 import { broadcastLanguages } from "../Utils/Const";
 
 
 const QuestionMessage = (props) => {
   const appContextlData = useContext(AppContext);
   const broadcastProgrammCode = appContextlData.broadcastProgramm.value;
-  // const broadcastLangCode = appContextlData.broadcastLang.value;
-  // const {
-  //   mqttUnSubscribe,
-  //   mqttSubscribe,
-  //   mqttPublush,
-  //   isConnected,
-  //   payload,
-  //   clientId,
-  // } = useMqtt();
-  // const [notificationList, setNotificationList] = useState([]);
-  // const mqttTopicList = broadcastLanguages.map((langItem, index) => {
-  //   const mqttTopic = `subtitles_${broadcastProgrammCode}_${langItem.value}`;
-  //   return mqttTopic;
-  // })
+  const broadcastLangCode = appContextlData.broadcastLang.value;
+  const {
+    mqttUnSubscribe,
+    mqttSubscribe,
+    mqttPublush,
+    isConnected,
+    payload,
+    clientId,
+  } = useMqtt();
+  const [notificationList, setNotificationList] = useState([]);
+  const mqttTopicList = broadcastLanguages.map((langItem, index) => {
+    const mqttTopic = `subtitles_${broadcastProgrammCode}_${langItem.value}`;
+    return mqttTopic;
+  })
 
-  // useEffect(() => {
-  //   // Notification.requestPermission();
+  useEffect(() => {
+    // Notification.requestPermission();
 
-  //   return () => {
-  //     mqttUnSubscribe(mqttTopic);
-  //   };
-  // }, []);
+    return () => {
+      mqttUnSubscribe(mqttTopic);
+    };
+  }, []);
 
-  // useEffect(() => {
-  //   if (isConnected) {
-  //     mqttTopicList.forEach((mqttTopic, index) => {
-  //       mqttSubscribe(mqttTopic);
-  //     })
-  //   }
-  // }, [isConnected]);
+  useEffect(() => {
+    if (isConnected) {
+      mqttTopicList.forEach((mqttTopic, index) => {
+        mqttSubscribe(mqttTopic);
+      })
+    }
+  }, [isConnected]);
 
+  useEffect(() => {
+    if (payload.message && mqttTopicList.includes(payload.topic)) {
+      const newMessage = JSON.parse(payload.message);
+      const notif = [newMessage];
+      setNotificationList(notif);
+      // new Notification(newMessage.slide);
 
+      if (newMessage && newMessage.clientId !== clientId) {
+        setJobMqttMessage(newMessage);
+      }
+    }
+  }, [payload]);
 
-  const mqttClient = appContextlData.mqttClient;
-  const setMqttClient = appContextlData.setMqttClient;
-  const setMqttClientId = appContextlData.setMqttClientId;
+  // const mqttClient = appContextlData.mqttClient;
+  // const setMqttClient = appContextlData.setMqttClient;
+  // const setMqttClientId = appContextlData.setMqttClientId;
   const [mqttQuestion, setMqttQuestion] = useState();
   const [mqttDate, setMqttDate] = useState();
 
@@ -53,24 +64,24 @@ const QuestionMessage = (props) => {
     broadcastLangMapObj[broadcastLangObj.value] = broadcastLangObj;
   });
 
-  function initMqttClient() {
-    if (!mqttClient) {
-      mqttClientUtils(setMqttClient, setMqttClientId);
-    }
+  // function initMqttClient() {
+  //   if (!mqttClient) {
+  //     mqttClientUtils(setMqttClient, setMqttClientId);
+  //   }
 
-    return mqttClient;
-  }
+  //   return mqttClient;
+  // }
 
-  function subscribeMqttMessage() {
-    if (mqttClient) {
-      const mqttTopicTmp = `${props.languageCode}_questions_${broadcastProgrammCode}`;
-      mqttClient.subscribe(mqttTopicTmp);
+  // function subscribeMqttMessage() {
+  //   if (mqttClient) {
+  //     const mqttTopicTmp = `${props.languageCode}_questions_${broadcastProgrammCode}`;
+  //     mqttClient.subscribe(mqttTopicTmp);
 
-      mqttClient.on("message", function (topic, message) {
-        updateMqttQuestion(message, props.languageCode);
-      });
-    }
-  }
+  //     mqttClient.on("message", function (topic, message) {
+  //       updateMqttQuestion(message, props.languageCode);
+  //     });
+  //   }
+  // }
 
   const updateMqttQuestion = (message) => {
     const messageStr = message.toString();
@@ -90,30 +101,6 @@ const QuestionMessage = (props) => {
     }
   };
 
-  useEffect(() => {
-    if (mqttClient) {
-      console.log(mqttClient);
-      subscribeMqttMessage();
-
-      mqttClient.on("connect", () => {
-        console.log("MQTT connected");
-      });
-
-      mqttClient.on("error", (err) => {
-        console.error("MQTT  connection error: ", err);
-        mqttClient.end();
-      });
-
-      mqttClient.on("reconnect", () => {
-        console.log("MQTT  reconnecting");
-      });
-
-      mqttClient.on("message", (topic, message) => {
-        updateMqttQuestion(message);
-      });
-    }
-  }, [mqttClient]);
-
   const parseUtcStrToLocal = (utcDateStr) => {
     let retVal = utcDateStr;
 
@@ -125,7 +112,7 @@ const QuestionMessage = (props) => {
     return retVal;
   };
 
-  initMqttClient();
+  i//nitMqttClient();
   if (mqttQuestion) {
     if (props.mode === "subtitle") {
       const langName = broadcastLangMapObj[props.languageCode].label;
@@ -143,14 +130,20 @@ const QuestionMessage = (props) => {
       );
     } else {
       return (
-        <div>
-          <li class="item">
-            <span class="datetime">Date: {parseUtcStrToLocal(mqttDate)}</span>
-            <br />
-            <div class="message">{mqttQuestion}</div>
-          </li>
-          <hr />
-        </div>
+        <>
+          {notificationList.map((obj) => (
+            <div>
+              <div>
+                <li class="item">
+                  <span class="datetime">Date: {parseUtcStrToLocal(mqttDate)}</span>
+                  <br />
+                  <div class="message">{mqttQuestion}</div>
+                </li>
+                <hr />
+              </div>
+            </div>
+          ))}
+        </>
       );
     }
   } else {
