acb44121613b4438e9bb1aaf05669b9688e836bd
Slava Pasechnik <slavapas13@gmail.com>
2025-02-16 06:27:00 +0200

    Remove DEBUG prefix

--- src/Utils/debugLog.js
+++ src/Utils/debugLog.js
@@ -2,7 +2,7 @@ const isLogMode = process.env.NODE_ENV === "development";
 
 const debugLog = (message, ...optionalParams) => {
   if (isLogMode) {
-    console.log(`[DEBUG]: ${message}`, ...optionalParams);
+    console.log(`${message}`, ...optionalParams);
   }
 };
 
